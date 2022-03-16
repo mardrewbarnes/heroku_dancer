9935d2a0e175fe963d463340c74000a26b53094e
mardrewbarnes <39124627+mardrewbarnes@users.noreply.github.com>
2022-03-17 09:26:35 +1100

    Test web service

--- app.pl
+++ app.pl
@@ -1,8 +1,34 @@
 #!/usr/bin/env perl
 use Dancer;
+#set serializer => 'XML';
+set serializer => 'JSON'; #un-comment this for json format responses
+ 
+get '/' => sub{
+    return {message => "First rest Web Service with Perl and Dancer"};
+};
 
-get '/' => sub {
-    "Hello Worlds!"
+get '/users/:name' => sub {
+    my $user = params->{name};
+    return {message => "Hello $user"};
+};
+ 
+get '/users' => sub{
+    my %users = (
+        userA => {
+            id   => "1",
+            name => "Carlos",
+        },
+        userB => {
+            id   => "2",
+            name => "Andres",
+        },
+        userC => {
+            id   => "3",
+            name => "Bryan",
+        },
+    );
+
+    return \%users;
 };
 
 dance;
