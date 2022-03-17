962c02cb20d2b92f05491482dff26e867d12ea35
mardrewbarnes <39124627+mardrewbarnes@users.noreply.github.com>
2022-03-17 11:00:10 +1100

    First attempt at Web service

--- app.pl
+++ app.pl
@@ -4,8 +4,17 @@ use Dancer;
 #set serializer => 'XML';
 set serializer => 'JSON'; #un-comment this for json format responses
  
+use Data::Dumper;
+use LWP::UserAgent;
+require HTTP::Request;
+my $request = HTTP::Request->new(POST => 'http://www.bom.gov.au/fwo/IDN60801/IDN60801.95765.json');
+
+
 get '/' => sub{
-    return {message => "First rest Web Service with Perl and Dancer"};
+	my $ua = LWP::UserAgent->new;
+	my $response = $ua->request($request);
+
+    return {message => "First rest Web Service with Perl and Dancer ".$response->{_content}};
 };
 
 get '/users/:name' => sub {
