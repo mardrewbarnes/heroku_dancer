8fbd3f6e77afb62061dce05346104d9392498e4b
mardrewbarnes <39124627+mardrewbarnes@users.noreply.github.com>
2022-03-17 11:41:49 +1100

    Call BOM web service

--- app.pl
+++ app.pl
@@ -5,16 +5,36 @@ use Dancer;
 set serializer => 'JSON'; #un-comment this for json format responses
  
 use Data::Dumper;
-use LWP::UserAgent;
-require HTTP::Request;
-my $request = HTTP::Request->new(POST => 'http://www.bom.gov.au/fwo/IDN60801/IDN60801.95765.json');
-
 
 get '/' => sub{
-	my $ua = LWP::UserAgent->new;
-	my $response = $ua->request($request);
+use LWP::UserAgent;
+require HTTP::Request;
 
-    return {message => "First rest Web Service with Perl and Dancer ".$response->{_content}};
+	my $request = HTTP::Request->new(POST => 'http://www.bom.gov.au/fwo/IDN60801/IDN60801.95765.json');
+	$request->push_header('Content-Type' => 'application/json');
+
+	my $ua = LWP::UserAgent->new;
+	my $uri = URI->new('http://www.bom.gov.au/fwo/IDN60801/IDN60801.95765.json');
+
+	# TODO:
+	# Need this otherwise the BOM returns an error about not allowing
+	# automated requests. Cannot use this in production, find out if we can
+	# get an account.
+	my $response = $ua->get($uri,
+		'Content-Type' => 'application/json',
+		'User-Agent' => 'PostmanRuntime/7.28.3',
+		'accept' => '*/*',
+		'accept-ranges' => 'bytes',
+		'content-encoding' => 'gzip, deflate, br',
+		'etag' => "b2efa-20a12-5da5ec685ed00",
+		'connection' => 'keep-alive',
+		'server' => 'Apache',
+		'vary' => 'Accept-Encoding',
+		'server-timing' => 'cdn-cache; desc=REVALIDATE'
+	);
+
+
+    return {message => $response->content};
 };
 
 get '/users/:name' => sub {
